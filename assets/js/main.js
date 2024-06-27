adcca781212622ce4fe0139eca20df63a36d7963
AndreaManuppelli <andreamanuppelli@gmail.com>
2024-06-27 18:14:41 +0200

    removed main.js

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-const contactModal = document.getElementById('contactModal')
-const modalOpener = document.getElementById('modalOpener')
-
-contactModal.addEventListener('shown.bs.modal', () => {
-  modalOpener.focus()
-})
